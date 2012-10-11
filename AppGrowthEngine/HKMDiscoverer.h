efdcae258024ce88594bf9836d9a1ba14f94d83c
Kirk Tsai <tsaik@yahoo.com>
2012-10-11 15:55:07 -0400

    Extracted String literals into define constants

--- AppGrowthEngine/HKMDiscoverer.h
+++ AppGrowthEngine/HKMDiscoverer.h
@@ -1,7 +1,43 @@
 #import <Foundation/Foundation.h>
 #import <MessageUI/MessageUI.h>
+#import "HKMLead.h"
 
 #define SDKVERSION @"IOS/1.1"
+#define MAX_ADDRESSBOOK_UPLOAD_SIZE 2000
+
+// SMS Verification notification
+#define NOTIF_HOOK_NOT_SMS_DEVICE @"HookNotSMSDevice"
+#define NOTIF_HOOK_DEVICE_VERIFIED @"HookDeviceVerified"
+#define NOTIF_HOOK_DEVICE_NOT_VERIFIED @"HookDeviceNotVerified"
+#define NOTIF_HOOK_VERIFY_DEVICE_COMPLETE @"HookVerifyDeviceComplete"
+#define NOTIF_HOOK_VERIFICATION_SMS_SENT @"HookVerificationSMSSent"
+#define NOTIF_HOOK_VERIFICATION_SMS_NOT_SENT @"HookVerificationSMSNotSent"
+
+// Addressbook discovery notification
+#define NOTIF_HOOK_DISCOVER_COMPLETE @"HookDiscoverComplete"
+#define NOTIF_HOOK_DISCOVER_NO_CHANGE @"HookDiscoverNoChange"
+#define NOTIF_HOOK_DISCOVER_FAILED @"HookDiscoverFailed"
+
+// Query invitation leads notification
+#define NOTIF_HOOK_QUERY_ORDER_COMPLETE @"HookQueryOrderComplete"
+#define NOTIF_HOOK_QUERY_ORDER_FAILED @"HookQueryOrderFailed"
+
+// Referral notification
+#define NOTIF_HOOK_NEW_REFERRAL_COMPLETE @"HookNewReferralComplete"
+#define NOTIF_HOOK_NEW_REFERRAL_FAILED @"HookNewReferralFailed"
+#define NOTIF_HOOK_UPDATE_REFERRAL_COMPLETE @"HookUpdateReferralComplete"
+#define NOTIF_HOOK_UPDATE_REFERRAL_FAILED @"HookUpdateReferralFailed"
+#define NOTIF_HOOK_QUERY_REFERRAL_COMPLETE @"HookQueryReferralComplete"
+#define NOTIF_HOOK_QUERY_REFERRAL_FAILED @"HookQueryReferralFailed"
+
+// Query friends with installs notification
+#define NOTIF_HOOK_QUERY_INSTALLS_COMPLETE @"HookQueryInstallsComplete"
+#define NOTIF_HOOK_QUERY_INSTALLS_FAILED @"HookQueryInstallsFailed"
+
+// other notifications
+#define NOTIF_HOOK_DOWNLOAD_SHARE_TEMPLATE_COMPLETE @"HookDownloadShareTemplatesComplete"
+#define NOTIF_HOOK_ADDRESSBOOK_CACHE_EXPIRED @"HookAddressbookCacheExpired"
+#define NOTIF_HOOK_NETWORK_ERROR @"HookNetworkError"
 
 @interface HKMDiscoverer : NSObject <MFMessageComposeViewControllerDelegate> {
     
@@ -65,6 +101,9 @@
     
     // Need to restore full screen after the SMS verification screen is displayed
     BOOL fullScreen;
+    
+    NSMutableDictionary *contactsDictionary;
+    
 }
 
 @property (nonatomic, retain) NSString *installCode;
@@ -111,7 +150,10 @@
 - (NSString *) getAddressbook:(int) limit;
 - (NSString *) getAddressbookHash:(int) limit;
 - (void) createVerificationSms;
-- (NSString *) lookupNameFromPhone:(NSString *)p;
+- (void) buildAddressBookDictionaryAsync;
+- (void) buildAddressBookDictionary;
+- (void) updateContactDetails:(HKMLead *)intoLead;
+//- (NSString *) lookupNameFromPhone:(NSString *)p;
 - (NSString *) formatPhone:(NSString *)p;
 
 - (BOOL) checkNewAddresses:(NSString *)ab;
